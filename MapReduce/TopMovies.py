27aa2089eb5355705cab772adeb82f1547657f7b
Mateus Mota <mateuspontesm@hotmail.com>
2018-08-17 01:43:11 -0300

    Adding Comments

--- MapReduce/TopMovies.py
+++ MapReduce/TopMovies.py
@@ -15,8 +15,8 @@ class TopMovies(MRJob):
         yield None, line
 
     def reducer2(self, _, values):
-        sort = sorted(values, reverse=True)
-        for i in range(10):
+        sort = sorted(values, reverse=True)  # sorting
+        for i in range(10):  # Top Ten Movies
             yield i + 1, sort[i][1]
 
     def steps(self):
